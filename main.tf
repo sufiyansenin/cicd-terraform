60feb5d69108606423bd06ab64c5de380c6b2696
Sufiyan <sufiyan95@hotmail.com>
2024-02-17 12:28:45 +0800

    17 Feb changes

--- main.tf
+++ main.tf
@@ -1,15 +1,3 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 resource "aws_sns_topic" "user_updates" {
   name = "sufiyan-updates-topic"
-}
-
-terraform {
-  backend "s3" {
-    bucket = "sctp-ce5-tfstate-bucket-1"
-    key    = "sufiyan.tfstate"
-    region = "us-east-1"
-  }
 }
